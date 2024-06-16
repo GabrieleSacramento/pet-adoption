ffe28aa5cd10197b4759b3e29cdb2d650779b403
GabrieleSacramento <gabisacramentoti@gmail.com>
2024-06-16 01:19:17 -0300

    project organization

---
+++ pet_doption_app/example/lib/src/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pet_doption_app/generated/app_localizations.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          restorationScopeId: 'app',
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('pt', ''), // Portuguese
+            // Other locales...
+          ],
+          onGenerateTitle: (BuildContext context) =>
+              AppLocalizations.of(context).appTitle,
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          home: Container()),
+    );
+  }
+}
---
+++ pet_doption_app/lib/generated/app_localizations_pt.dart
@@ -0,0 +1,9 @@
+import 'app_localizations.dart';
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get appTitle => 'pet_adoption_app';
+}
